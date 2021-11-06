bdfdc7ba0980114203f743a63ee094ed5a711c89
Carl Williams <carl@carlpwilliams.com>
2021-11-06 08:50:46 +0000

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,42 @@
-import express from 'express';
-import { ProfitTrailer } from './profitTrailer';
-import config from '../config.json';
-import { Discord } from './discord';
+import express from "express";
+import { ProfitTrailer } from "./profitTrailer";
+import config from "../config.json";
+import { Discord } from "./discord";
 
-const app = express()
+const app = express();
 const port = config.port;
 
 let profitTrailer: ProfitTrailer;
 let discord: Discord;
 
 const ptLoaded = (pt: ProfitTrailer) => {
-  console.info('pt reported it loaded!');
+  console.info("pt reported it loaded!");
   discord = new Discord(pt);
   discord.start();
-}
+};
 
 const init = () => {
   profitTrailer = new ProfitTrailer(ptLoaded);
-}
-app.use('/', express.static('public'))
+};
+app.use("/", express.static("public"));
 
-app.get('/getpositions', async (req: any, res: any) => {
-  console.info('get positions');
+app.get("/getpositions", async (req: any, res: any) => {
+  console.info("get positions");
   const positions: any = await profitTrailer.getPositions();
   res.send(positions);
-})
+});
 
-app.get('/durations', async (req: any, res: any) => {
-  console.info('get trade times');
+app.get("/durations", async (req: any, res: any) => {
+  console.info("get trade times");
   const positions: any = await profitTrailer.getTradeDurations();
   res.send(positions);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  if (config.optimisePTDB) {
+    profitTrailer.prepareStats();
+  }
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 init();
